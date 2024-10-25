9d3bb1765c85026bdc3348df7f068255439ee086
jpyepes <jpyg123@gmail.com>
2024-10-25 00:57:06 -0500

    Create api-gateway with docker

---
+++ service1/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;  // Cambia a 3002 en service2
+
+app.get('/', (req, res) => {
+  res.send(`Hello from ${process.env.SERVICE_NAME}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Service running on port ${PORT}`);
+});
